f906d9a1536c77501dc5d254472c4d130b104846
thomasnilsson <s144470@student.dtu.dk>
2018-01-29 12:03:25 +0100

    week 1 done

---
+++ 02806/script.js
@@ -0,0 +1,19 @@
+let body = d3.select("body")
+let content = body.select("#panelDiv")
+
+let addButton = (weekNumber) => content.append("a")
+	.attr("class", "btn btn-primary btn-block")
+	.text("Week " + weekNumber)
+	.attr("href", "/week" + weekNumber)
+
+let weeks = [1]
+
+for (i in weeks) {
+	let w = weeks[i]
+	addButton(w)
+}
+
+
+
+
+
