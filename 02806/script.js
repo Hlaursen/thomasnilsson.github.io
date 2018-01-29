dcbc150d489983686d9949409a4b0624c113e90a
thomasnilsson <s144470@student.dtu.dk>
2018-01-29 12:16:33 +0100

    Update script.js

--- 02806/script.js
+++ 02806/script.js
@@ -4,7 +4,7 @@ let content = body.select("#panelDiv")
 let addButton = (weekNumber) => content.append("a")
 	.attr("class", "btn btn-primary btn-block")
 	.text("Week " + weekNumber)
-	.attr("href", "/week" + weekNumber + "/index.html")
+	.attr("href", "02806/week" + weekNumber)
 
 let weeks = [1]
 
