7a0a0bc943285fc43bfc1f28a269e0c8c4887c1a
thomasnilsson <s144470@student.dtu.dk>
2018-02-13 17:30:12 +0100

    week 3 in progress

--- 02806/week3/js/barchart.js
+++ 02806/week3/js/barchart.js
@@ -1,13 +1,14 @@
-let chartHeight = 200
-let chartWidth = 500
+let h = 400
+let w = 1000
+let padding = 25
 let body = d3.select("body")
 let content = body.selectAll()
 let barChartDiv = body.select("#barChartDiv")
 
 let barSvg = barChartDiv
 	.append("svg")
-	.attr("width", chartWidth)
-	.attr("height", chartHeight)
+	.attr("width", w)
+	.attr("height", h)
 
 let addParagraph = (c,t) => c
 	.append("p")
@@ -16,18 +17,8 @@ let addParagraph = (c,t) => c
 let randomValuesX = function(l) {
 	let numbers = []
 	for (i = 1; i <= l; i++){
-		let x = Math.round(Math.random() * 1000)
+		let x = Math.round(Math.random() * h)
 		let obj = { "x" : x }
-		numbers.push(obj)
-	}
-	return numbers
-}
-
-let randomValuesY = function(l) {
-	let numbers = []
-	for (i = 1; i <= l; i++){
-		let x = Math.round(Math.random() * 800)
-		let obj = { "y" : x }
 		numbers.push(obj)
 	}
 	return numbers
@@ -38,47 +29,56 @@ let animateChart = function(jsonData) {
 	barSvg.selectAll("rect").remove()
 	barSvg.selectAll("text").remove()
 
-	let xMax = d3.max(jsonData, p => p.x)
-
-	console.log(xMax)
-
-	let range = d3.range(jsonData.length)
+	let N = jsonData.length
 
 	let xScale = d3.scaleBand()
-		.domain(range)
-		.range([0, w])
+		.domain(d3.range(N))
+		.rangeRound([padding, w - padding])
 		.paddingInner(0.05)
 
-	let xAxis = d3.axisBottom(xScale).ticks(5)
+	let xAxis = d3.axisBottom(xScale)
 
+	let yScale = d3.scaleLinear()
+		.domain([0, h])
+		.range([h - padding, padding])
 
-	// Draw a vertical rectangle for each datapoint
+	let yAxis = d3.axisLeft(yScale).ticks(5)
+
+	// Bars
 	barSvg.selectAll("rect")
 		.data(jsonData)
 		.enter()
 		.append("rect")
-		.attr("x", (d,i) => i * deltaX)
-		.attr("y", d => chartHeight - d.x)
-		.attr("width", barWidth)
-		.attr("height", d => d.x)
-		.attr("fill", d => "rgb(" + Math.round(d.x * 2) + ", 0, 0)")
+		.attr("x", (d,i) => xScale(i))
+		.attr("y", d => yScale(h - d.x) - padding)
+		.attr("width", xScale.bandwidth())
+		.attr("height", d => yScale(d.x))
+		.attr("fill", d => "rgb(40,40," + Math.round(d.x * 5) + ")")
 
-	// Draw values of each bar as text
-	barSvg.selectAll("text")
-		.data(jsonData)
-		.enter()
-		.append("text")
-		.text(d => d.x)
-		.attr("fill", "white")
-		.attr("font-size", "8")
-		.attr("x", (d,i) => i * deltaX + 3)
-		.attr("y", (d,i) => chartHeight - d.x + 12)
+		// Text
+		barSvg.selectAll("text")
+			.data(jsonData)
+			.enter()
+			.append("text")
+			.text(d => d.x)
+			.attr("fill", "white")
+			.attr("font-size", "12")
+			.attr("x", (d,i) => xScale(i) + xScale.bandwidth() / 3)
+			.attr("y", (d,i) => h - yScale(d.x) - 8)
 
+		// Axes
+		barSvg.append("g")
+			.attr("transform", "translate(0, " + (h - padding) + ")")
+			.call(xAxis)
+
+		barSvg.append("g")
+			.attr("transform", "translate(" + padding + ", 0)")
+			.call(yAxis)
 }
 
 let drawChart = function() {
-	let length = 25
-	// let dataset = randomArray(length, chartHeight)
+	let length = 20
+	// let dataset = randomArray(length, h)
 	let dataset = randomValuesX(length)
 	animateChart(dataset)
 }
@@ -86,19 +86,9 @@ let drawChart = function() {
 let drawChartCSV = function() {
 	d3.csv('data/x_data.csv', function(jsonData) {
 		animateChart(jsonData)
-	})	
+	})
 }
 
 drawChart()
 
 addParagraph(barChartDiv, "Figure: A bar chart")
-
-
-
-
-
-
-
-
-
-
