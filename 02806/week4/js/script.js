23954055be5c884fd9aae840402d366f6c4dafc8
thomasnilsson <s144470@student.dtu.dk>
2018-02-20 15:21:30 +0100

    Tooltips + assignment folder

--- 02806/week4/js/script.js
+++ 02806/week4/js/script.js
@@ -4,7 +4,18 @@ let padding = 25
 let height = h - padding
 let width = w - padding
 let animationTime = 2000
-let histogramDiv = d3.select("body").select("#histogramDiv")
+let body = d3.select("body")
+let histogramDiv = body.select("#histogramDiv")
+
+// let tooltipDiv = body.append("div")
+//     .attr("class", "tooltip")
+//     .style("opacity", 0)
+// let tooltipPointer = body.append("div")
+//     .attr("class", "arrow-down")
+//     .style("opacity", 0)
+
+
+let INNER_PADDING = 0.1
 
 let svg = histogramDiv
 	.append("svg")
@@ -25,9 +36,53 @@ let TITLES = ["Fresh Fruit", "Storage Fruit", "Fresh Vegetable", "Storage Vegeta
 let MONTHS, ALL_DATA = []
 let x, y, xAxis, yAxis
 
-d3.csv("data/nyDATA.csv", csvData => {
+let handleMouseOver = (rect, d) => {
+	// Log mouse coord
+	console.log(d3.event.clientX, d3.event.clientY)
 
-  histogramDiv.append("h4").text(TITLES[DATA_INDEX])
+	// var xPosition = parseFloat(d3.select(rect).attr("x")) + width/2
+	// var yPosition = parseFloat(d3.select(rect).attr("y")) / 2 + height
+
+	let xPosition = d3.event.clientX - 40
+	let yPosition = d3.event.clientY - 40
+	//Update the tooltip position and value
+    d3.select("#tooltip")
+	      .style("left", xPosition + "px")
+	      .style("top", yPosition + "px")
+
+		d3.select("#count").text(d.Count)
+    d3.select("#month").text(d.Month)
+    //Show the tooltip
+		d3.select("#tooltip").classed("hidden", false);
+	// tooltipDiv.transition().style("opacity", 0.95)
+	// tooltipDiv.html("<strong>" + d.Month + "</strong><br>" + d.Count)
+  //  .style("left", xPos + "px")
+  //  .style("top", yPos + "px")
+  //
+	// tooltipPointer.transition().style("opacity", 0.95)
+	// tooltipPointer.style("left", xPos + 15 + "px").style("top", yPos + 40 + "px")
+
+	d3.select(rect)
+		.attr("fill", "orange")
+}
+
+let handleMouseOut = rect => {
+	//Hide the tooltip
+	d3.select("#tooltip").classed("hidden", true);
+	// tooltipDiv.transition().style("opacity", 0.0)
+	// tooltipPointer.transition().style("opacity", 0.0)
+	d3.select(rect)
+		.transition()
+		.duration(250)
+		.attr("fill", COLORS[DATA_INDEX])
+  //
+	// d3.select("#tooltip").remove()
+}
+
+d3.csv("data/nydata.csv", csvData => {
+  histogramDiv
+		.append("h4")
+			.text(TITLES[DATA_INDEX])
   // parse data
   ALL_DATA = csvData.map(d => parseRow(d))
 
@@ -40,7 +95,7 @@ d3.csv("data/nyDATA.csv", csvData => {
   x = d3.scaleBand()
     .domain(MONTHS)
     .rangeRound([padding, width])
-    .paddingInner(0.5)
+    .paddingInner(INNER_PADDING)
 
   xAxis = d3.axisBottom(x)
 
@@ -55,11 +110,17 @@ d3.csv("data/nyDATA.csv", csvData => {
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d,i) => x(d.Month))
+    .attr("x", d => x(d.Month))
     .attr("y", d =>  y(d.Count))
     .attr("width", x.bandwidth())
     .attr("height", d => height - y(d.Count))
-    .attr("fill", d => COLORS[DATA_INDEX])
+    .attr("fill", COLORS[DATA_INDEX])
+		.on("mouseover", function(d) {
+			handleMouseOver(this, d)
+		})
+		.on("mouseout", function() {
+			handleMouseOut(this)
+		})
 
     // Axes
     svg.append("g")
@@ -91,10 +152,10 @@ let updateChart = () => {
     .data(data)
     .transition()
     .duration(animationTime)
-    .attr("x", (d,i) => x(d.Month))
+    .attr("x", d => x(d.Month))
     .attr("y", d =>  y(d.Count))
     .attr("height", d => height - y(d.Count))
-    .attr("fill", d => COLORS[DATA_INDEX])
+    .attr("fill", COLORS[DATA_INDEX])
 
     // Update y axis only
     svg.selectAll("#yAxis").call(yAxis)
